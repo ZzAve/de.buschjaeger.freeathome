fc71bdf119afd2dceec7f517072b0df7b495ab6e
jvandis <jvandis@bol.com>
2020-10-02 23:17:39 +0200

    Fix inconsistent behaviour for blinds

--- drivers/blind/device.js
+++ drivers/blind/device.js
@@ -11,52 +11,65 @@ class Blind extends FreeAtHomeDevice {
 
     this.registerMultipleCapabilityListener(
       capabilities,
-      this.onMultipleCapabilities.bind(this)
+      this.onMultipleCapabilities.bind(this),
+      500
     );
-
-    // this.addCapability('windowcoverings_state')
-    // 	.catch(this.error)
-    // 	.then(this.log);
   }
 
-  async onMultipleCapabilities(valueObj, optsObj) {
+  onMultipleCapabilities(valueObj, optsObj) {
     this.debug("valueObj", valueObj);
     this.debug("optsObj", optsObj);
 
     const convertedValue = {};
     // Calculate/Convert capabilities value
     if (typeof valueObj.windowcoverings_set === "number") {
-      convertedValue.windowcoverings_set = ((1 - valueObj.windowcoverings_set) * 100).toFixed(0);
+      convertedValue.windowcoverings_set = (
+        (1 - valueObj.windowcoverings_set) *
+        100
+      ).toFixed(0);
     }
 
-    if (typeof valueObj.windowcoverings_state === "string"){
-    	convertedValue.windowcoverings_state = this.toFreeAtHomeDirection(valueObj.windowcoverings_state);
-		}
+    if (typeof valueObj.windowcoverings_state === "string") {
+      convertedValue.windowcoverings_state = this.toFreeAtHomeDirection(
+        valueObj.windowcoverings_state
+      );
+    }
 
     const promises = [];
-    if (typeof convertedValue.windowcoverings_set !== "undefined") {
+
+    if (typeof convertedValue.windowcoverings_state !== "undefined") {
+      promises.push(
+        this.handleCapability(
+          convertedValue.windowcoverings_state,
+          optsObj.windowcoverings_state,
+          "windowcoverings_state"
+        ).then(_ => {
+          this.setCapabilityValue(
+            "windowcoverings_state",
+            valueObj.windowcoverings_state
+          ).catch(this.error);
+        })
+      );
+    } else if (typeof convertedValue.windowcoverings_set !== "undefined") {
       promises.push(
         this.ensureDeviceIsNotMoving()
           .then(_ =>
-            this.handleCapability(convertedValue.windowcoverings_set, optsObj.windowcoverings_set, "windowcoverings_set")
+            this.handleCapability(
+              convertedValue.windowcoverings_set,
+              optsObj.windowcoverings_set,
+              "windowcoverings_set"
+            )
           )
           .then(() => {
-            this.setCapabilityValue("windowcoverings_set", valueObj.windowcoverings_set).catch(this.error);
+            this.setCapabilityValue(
+              "windowcoverings_set",
+              valueObj.windowcoverings_set
+            ).catch(this.error);
           })
       );
     }
 
-		if (typeof convertedValue.windowcoverings_state !== "undefined") {
-			promises.push(
-				this.handleCapability(convertedValue.windowcoverings_state, optsObj.windowcoverings_state, "windowcoverings_state")
-					.then(_ => {
-						this.setCapabilityValue("windowcoverings_state", valueObj.windowcoverings_state).catch(this.error);
-					})
-			)
-		}
-
-
-    await Promise.all(promises);
+    return Promise.all(promises);
   }
 
   async ensureDeviceIsNotMoving() {
@@ -78,40 +91,41 @@ class Blind extends FreeAtHomeDevice {
     this._updateState(safe(changedState).deviceUpdate);
   }
 
-  toFreeAtHomeDirection(homeyDirection){
-		switch (homeyDirection) {
-			case "down":
-				return 1;
-			case "up":
-				return 0;
-			default:
-				// if device is already idle, don't move it
-				return this.moveDirection === 0 ? undefined : "0";
-		}
-	}
+  toFreeAtHomeDirection(homeyDirection) {
+    switch (homeyDirection) {
+      case "down":
+        return 1;
+      case "up":
+        return 0;
+      default:
+        // if device is already idle, don't move it
+        return this.moveDirection === 0 ? undefined : "0";
+    }
+  }
+
   toMovingDirection(freeAtHomeDirection) {
-		switch (freeAtHomeDirection) {
-			case "3":
-				return "down";
-			case "2":
-				return "up";
-			default:
-				return "idle";
-		}
-	}
+    switch (freeAtHomeDirection) {
+      case "3":
+        return "down";
+      case "2":
+        return "up";
+      default:
+        return "idle";
+    }
+  }
 
   _updateState(deviceState) {
     const data = deviceState.channels[this.deviceChannel].datapoints;
 
     /*
-		odp0000 -- pairingid 288 move indication
-		odp0001 -- location indication
+      odp0000 -- pairingid 288 move indication
+      odp0001 -- location indication
 
-		idp0000 -- move in direction (1 is down, 0 is up)
-		idp0001 -- stop / start moving in direction (1 is down, 0 is up)
-		idp0002 --  35 location indiccation
-
-		 */
+      idp0000 -- move in direction (1 is down, 0 is up)
+      idp0001 -- stop / start moving in direction (1 is down, 0 is up)
+      idp0002 --  35 location indiccation
+        
+    */
 
     const movingDirection = data["odp0000"];
     const locationIndication = data["odp0001"];
@@ -122,7 +136,9 @@ class Blind extends FreeAtHomeDevice {
         `Setting ${this.id}  windowcoverings_set to ${convertedValue} (derived from ${locationIndication.value})`
       );
       this.setStateSafely(convertedValue, "windowcoverings_set");
-    } else if ("value" in movingDirection) {
+    }
+
+    if ("value" in movingDirection) {
       const convertedDirection = this.toMovingDirection(movingDirection.value);
       this.moveDirection = +movingDirection.value;
       this.log(
